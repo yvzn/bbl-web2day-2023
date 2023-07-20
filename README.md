25bd3740a802e8878eed2209f4d7f1c9cf625abd
yvzn <mail@ludeo.net>
2023-07-20 11:28:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bbl-web2day-2023
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-z9wvna)
---
+++ main.js
@@ -0,0 +1,8 @@
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/night.css'
+
+import './style.css'
+
+import Reveal from 'reveal.js'
+
+Reveal.initialize({ hash: true })
